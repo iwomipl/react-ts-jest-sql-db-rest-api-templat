7e7de2284b8071f7f0deab68e3e240de4e8f1fa2
Iwo Matecki <iwo@iwomi.pl>
2022-05-21 19:43:38 +0200

    Add usage of types from backend (my Express-TS-JEST-SQL-DB-REST-API-template  app)

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const {override} = require('customize-cra');
+const {aliasDangerous, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous');
+
+module.exports = {
+    webpack: override(
+        aliasDangerous(configPaths('./tsconfig.paths.json'))
+    ),
+}
+
+//This is basically  configuration for my app to be able to find types from another TS app

b83b62e11219598a0a51d0c6479b814669101a04
Iwo Matecki <iwo@iwomi.pl>
2022-06-01 13:07:16 +0200

    Reorganize and add dynamic creating header and body into fetchFunction

--- src/utils/fetchFunction.ts
+++ src/utils/fetchFunction.ts
@@ -1,23 +1,51 @@
-export const fetchFunction = async (path: string, formObject: { login: string, password: string }) => {
+export const fetchFunction = async (path: string, methodFromCall: string, formObject?: { login: string, password: string }) => {
     const tokenObj = localStorage.getItem('token')
-        ?  localStorage.getItem('token')
+        ? localStorage.getItem('token')
         : null;
-    const headersObj: {} = tokenObj
-        ? {
-        'Content-Type': 'application/json',
-    }
-    : {
+    const bodyObj = formObject ? JSON.stringify(formObject) : null;
+    let headersObj: {};
+    if (!tokenObj) {
+        headersObj = {
+            'Content-Type': 'application/json',
+        }
+    } else {
+        headersObj = {
             'Content-Type': 'application/json',
             Authorization: tokenObj,
         }
-    const result = await fetch(`http://localhost:3001/${path}`, {
-        method: 'POST',
-        headers: headersObj,
-        body: JSON.stringify(formObject),
-    });
-    const data = await result.json();
-    if (data.token){
-        localStorage.setItem('token', data.token);
     }
-    return data;
+    const fetchObj = bodyObj
+        ?{
+        method: methodFromCall,
+        headers: headersObj,
+        body: bodyObj,
+    } :{
+            method: methodFromCall,
+            headers: headersObj,
+        };
+    try {
+        const result = await fetch(`http://localhost:3001/${path}`, fetchObj);
+        if (result.status === 200 || result.status === 401) {
+            const data = await result.json();
+            if (data.token) {
+                    localStorage.setItem('token', data.token);
+            }
+            if (result.status === 401) {
+                console.log('here')
+                localStorage.removeItem('token');
+            }
+            return data;
+        } else {
+            return {
+                message: 'You Are Not Logged In',
+                "loginStatus": false,
+            }
+        }
+    }catch(err){
+        return {
+            message: 'You Are Not Logged In',
+            "loginStatus": false,
+        }
+
+    }
 }
